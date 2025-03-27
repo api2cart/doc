0cd13a52d0251bf6f83fdec1ffff9c692de67002
y.klymyk <y.klymyk@AESON>
2025-03-27 14:48:19 +0200

    1

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -5,26 +5,26 @@
   let lang = pathSegments[1]; // "de" або "it" чи "fr"...
 
   // Шлях до файлу openapi за замовчуванням (англійський)
-  let openapiUrl = "https://api2cart-5448ee.gitlab.io/openapi.json";
+  let openapiUrl = "https://api2cart.github.io/doc/openapi.json";
 
   // Залежно від lang підставляємо іншу URL-адресу
   switch (lang) {
     case 'de':
-      openapiUrl = "https://api2cart-5448ee.gitlab.io/de/de_openapi.json";
+      openapiUrl = "https://api2cart.github.io/doc/de/openapi.json";
       break;
     case 'it':
-      openapiUrl = "https://api2cart-5448ee.gitlab.io/it/it_openapi.json";
+      openapiUrl = "https://api2cart.github.io/doc/it/openapi.json";
       break;
     case 'fr':
-      openapiUrl = "https://api2cart-5448ee.gitlab.io/fr/fr_openapi.json";
+      openapiUrl = "https://api2cart.github.io/doc/fr/openapi.json";
       break;
     case 'es':
-      openapiUrl = "https://api2cart-5448ee.gitlab.io/es/es_openapi.json";
+      openapiUrl = "https://api2cart.github.io/doc/es/openapi.json";
       break;
     // Можна додати інші мови / кейси
     default:
       // якщо lang порожнє або не співпадає
-      openapiUrl = "https://api2cart-5448ee.gitlab.io/openapi.json";
+      openapiUrl = "https://api2cart.github.io/doc/openapi.json";
   }
 
   // Ініціалізуємо Swagger
