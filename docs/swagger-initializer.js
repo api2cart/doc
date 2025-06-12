d982406241f051b9aa710b8e113513058f79c74c
Yurii Klymyk <y.klymyk@api2cart.com>
2025-06-12 11:13:29 +0300

    2

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,33 +1,15 @@
-// Приклад: https://api2cart-5448ee.gitlab.io/de/index.html
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: https://api2cart-5448ee.gitlab.io/de/index.html
 // window.location.pathname => "/de/index.html"
-// Розіб'ємо шлях на складові: ['', 'de', 'index.html']
+// пїЅпїЅпїЅпїЅ'пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: ['', 'de', 'index.html']
 const pathSegments = window.location.pathname.split('/');
-let lang = pathSegments[2]; // "de" або "it" чи "fr"...
+let lang = pathSegments[2]; // "de" пїЅпїЅпїЅ "it" пїЅпїЅ "fr"...
 
-// Шлях до файлу openapi за замовчуванням (англійський)
-let openapiUrl = "https://api2cart.github.io/doc/openapi.json";
+// пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ openapi пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ (пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ)
+let openapiUrl = "openapi.json";
 
-// Залежно від lang підставляємо іншу URL-адресу
-switch (lang) {
-  case 'de':
-    openapiUrl = "https://api2cart.github.io/doc/de/openapi.json";
-    break;
-  case 'it':
-    openapiUrl = "https://api2cart.github.io/doc/it/openapi.json";
-    break;
-  case 'fr':
-    openapiUrl = "https://api2cart.github.io/doc/fr/openapi.json";
-    break;
-  case 'es':
-    openapiUrl = "https://api2cart.github.io/doc/es/openapi.json";
-    break;
-  // Можна додати інші мови / кейси
-  default:
-    // якщо lang порожнє або не співпадає
-    openapiUrl = "https://api2cart.github.io/doc/openapi.json";
-}
 
-// Ініціалізуємо Swagger
+
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Swagger
 window.ui = SwaggerUIBundle({
   url: openapiUrl,
   dom_id: '#swagger-ui',
